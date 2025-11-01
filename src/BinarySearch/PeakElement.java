cac53d1bf00466077d8c85b551e76da1924b141d
artemis-exo <vedaantbisht@gmail.com>
2025-11-01 23:29:54 +0530

    Peek approach2 and Multiple peek

--- src/BinarySearch/PeakElement.java
+++ src/BinarySearch/PeakElement.java
@@ -2,13 +2,23 @@ package BinarySearch;
 
 public class PeakElement {
     public static void main(String[] args) {
-        int [] arr={1,2,4,5,6,2,3,4};
+        int [] arr={1,2,3,4,5};
+        int [] arr1={1,2,1,3,4,2,5,1};
+        System.out.println(MorePeeks(arr1));
         System.out.println(Peak(arr));
+        System.out.println(peek2(arr));
 
     }
     static  int Peak(int [] arr){
-        int start=0;
-        int end= arr.length;
+        int n= arr.length;
+        if (n==1)
+            return 0;
+        if(arr[0]>arr[1])
+            return 0;
+        if(arr[n-1]>arr[n-2])
+            return n-1;
+        int start=1;
+        int end= arr.length-1;
         int mid;
         while(start<end){
             mid=start+(end-start)/2;
@@ -29,4 +39,62 @@ public class PeakElement {
         // and if we are saying that only one item is remaining, hence cuz of above line that is the best possible ans
         return start;
     }
+    // Peak elements approach 2
+    static  int peek2(int[] arr){
+        int n= arr.length;
+        if (n==1)
+            return 0;
+        if(arr[0]>arr[1])
+            return 0;
+        if(arr[n-1]>arr[n-2])
+            return n-1;
+        int start=1;
+        int end= arr.length-2;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+            if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+                return mid;
+            }
+            else if(arr[mid]>arr[mid+1]){
+                end=mid-1;
+            }
+            else if(arr[mid]>arr[mid-1]){
+                start=mid+1;
+            }
+        }
+        return -1;
+
+    }
+    static  int MorePeeks(int[] arr){
+        int n= arr.length;
+        if (n==1)
+            return 0;
+        if(arr[0]>arr[1])
+            return 0;
+        if(arr[n-1]>arr[n-2])
+            return n-1;
+        int start=1;
+        int end= arr.length-2;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+            if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+                return mid;
+            }
+            else if(arr[mid]>arr[mid+1]){
+                end=mid-1;
+            }
+            else if(arr[mid]>arr[mid-1]){
+                start=mid+1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+
 }
